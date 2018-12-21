c6d3c5f39af9c2dd66d4ee8b2f1b125c44f8a46f
Gustav Pursche <pursche@posteo.de>
2018-12-21 22:03:15 +0100

    Create average or first column

--- functions/read.js
+++ functions/read.js
@@ -2,14 +2,21 @@ import fetch from 'node-fetch';
 
 exports.handler = (event, context, callback) => {
   const { SHEET_URL, SHEET_API_KEY } = process.env;
-  const RANGE = 'A1';
+  const RANGE = 'A1:A';
 
   fetch(`${SHEET_URL}/values/${RANGE}?key=${SHEET_API_KEY}`)
     .then(res => res.json())
     .then(data => {
+      const { values } = data;
+      const average = values.reduce((acc, current) => {
+        // eslint-disable-next-line no-param-reassign
+        acc += parseInt(current[0], 10);
+        return acc;
+      }, 0);
+
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: JSON.stringify({ average })
       });
     });
 };
