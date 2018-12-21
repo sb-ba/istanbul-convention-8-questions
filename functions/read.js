557acc8acc3b5dcae6484d9a8dd9f90e5bd47d3b
Gustav Pursche <pursche@posteo.de>
2018-12-21 22:25:50 +0100

    Extend range

--- functions/read.js
+++ functions/read.js
@@ -2,22 +2,36 @@ import fetch from 'node-fetch';
 
 exports.handler = (event, context, callback) => {
   const { SHEET_URL, SHEET_API_KEY } = process.env;
-  const RANGE = 'A1:A';
+  const RANGE = 'A:C';
 
   fetch(`${SHEET_URL}/values/${RANGE}?key=${SHEET_API_KEY}`)
     .then(res => res.json())
     .then(data => {
       const { values } = data;
-      const average =
-        values.reduce((acc, current) => {
-          // eslint-disable-next-line no-param-reassign
-          acc += parseInt(current[0], 10);
-          return acc;
-        }, 0) / values.length;
+      const averages = values.reduce((acc, current) => {
+        current.forEach((column, index) => {
+          if (!acc[index]) {
+            acc[index] = {
+              count: 0,
+              total: 0
+            };
+          }
+
+          acc[index].count += 1;
+          acc[index].total += parseInt(column, 10);
+        });
+
+        return acc;
+      }, {});
+
+      Object.keys(averages).forEach(index => {
+        averages[index].average = averages[index].total / averages[index].count;
+        delete averages[index].total;
+      });
 
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify({ count: values.length, average })
+        body: JSON.stringify({ count: values.length, averages })
       });
     });
 };
