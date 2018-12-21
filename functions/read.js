5d8ec9632ec688c712ee1e382f47639eb8e93f99
Gustav Pursche <pursche@posteo.de>
2018-12-21 23:08:35 +0100

    Flatten results

--- functions/read.js
+++ functions/read.js
@@ -31,7 +31,7 @@ exports.handler = (event, context, callback) => {
 
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify({ averages })
+        body: JSON.stringify({ ...averages })
       });
     });
 };
